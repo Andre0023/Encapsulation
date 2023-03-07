e209b41b6d3058fe6709641ab8263bd2c2f426a2
Андрей <Vash0023@gmail.com>
2023-03-07 21:12:05 +0300

    Исправил копирование объектов

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Elevator elevator = new Elevator(-3, 26);
-        while (true) {
-            System.out.print("Введите номер этажа: ");
-            int floor = new Scanner(System.in).nextInt();
-            elevator.move(floor);
-        }
+
 
 
         Dimensions dimensions = new Dimensions(3,4,8); //создаю объект класса Dimensions.
-        Dimensions copy = new Dimensions(4,3,6);
+        Dimensions copy = dimensions.setWidth(9);
+        System.out.println(dimensions.getWidth() + " Адрес" + dimensions);
+        System.out.println(copy.getWidth()+ " Адрес" + copy);
 
 
         Cargo cargo = new Cargo(dimensions,10,"Melaxen, d 43, of 9", true, "2083507797688", true );
         System.out.println(cargo);
         System.out.println();
-        cargo = cargo.setWeight(15); //меняю параметр  класса Cargo
-        System.out.println(cargo);
+        Cargo cargo1 = cargo.setWeight(15); //меняю параметр  класса Cargo
+        System.out.println(cargo.getWeight());
+        System.out.println(cargo1.getWeight());
+
+//        Elevator elevator = new Elevator(-3, 26);
+//        while (true) {
+//            System.out.print("Введите номер этажа: ");
+//            int floor = new Scanner(System.in).nextInt();
+//            elevator.move(floor);
+//        }
 
 
 
